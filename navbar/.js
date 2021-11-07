5efa395976b14c99ecb3e4a57b3792fe0a341b23
arwds <arowadas@gmail.com>
2021-11-07 16:33:39 -0500

    navbar completed and nvbar

---
+++ navbar/.js
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    let nav = document.querySelector('.navbar');
+    nav.innerHTML=`
+    <div class="navdiv">
+            <img src="/Arowadas/fulllogo.png" class="logo" alt="Navbar Logo">
+            <div class="navthings">
+                <div class="search">
+                    <input type="text" class="searchbar" placeholder="Search items">
+                    <button class="searchbutton"><b>Search</b></button>
+                </div>
+            </div>
+    </div>
+    <ul class="linklist">
+        <li class="link"><a href="/Arowadas/index.html">Home</a></li>
+        <li class="link"><a href="/Arowadas/compo/index.html">Compo</a></li>
+        <li class="link"><a href="/Arowadas/membercard/index.html">Member Card</a></li>
+    </ul>
+    `;
+}
+
+Navbar();

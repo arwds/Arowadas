604daac81cd66b9862f4a93b0ff1b2e83263cdc4
arwds <arowadas@gmail.com>
2022-01-16 09:20:39 -0500

    New tabs added based on subsidiary

--- navbar/script.js
+++ navbar/script.js
@@ -16,6 +16,8 @@ const Navbar = () => {
         <li class="link"><a href="/mathcompo">Math Compo</a></li>
         <li class="link"><a href="/mustacheman">Mustache Man</a></li>
         <li class="link"><a href="/sparkl">Sparkl</a></li>
+        <li class="link"><a href="/song">Song Group</a></li>
+        <li class="link"><a href="/news">ANEWS</a></li>
     </ul>
     <script src="/navbar/searchbar.js"></script>
     `;

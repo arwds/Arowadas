b73127331160e2799628e55f4160ad04be7f2505
arwds <arowadas@gmail.com>
2022-05-14 16:00:47 -0400

    addedabout

--- footer/script.js
+++ footer/script.js
@@ -6,8 +6,9 @@ const Footer = () => {
     </div>
     <div id="main">
         <a class="footerlink" href="mailto:contactus.arowadas@gmail.com">Contact Us</a><br>
-        <a class="footerlink" href="#">Privacy Policy</a><br>
-        <a class="footerlink" href="#">Terms of Service</a>
+        <a class="footerlink" href="/policies/privacypolicy.html">Privacy Policy</a><br>
+        <a class="footerlink" href="/policies/termsofservice.html">Terms of Service</a>
+        <a class="footerlink" href="/policies/about.html">About</a>
     </div>
     `;
 }

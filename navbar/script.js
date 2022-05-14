678bc51d658c11399ef389b00aac889a0bdf8576
arwds <arowadas@gmail.com>
2022-05-13 20:52:20 -0400

    added and subtracted things:)

--- navbar/script.js
+++ navbar/script.js
@@ -3,12 +3,6 @@ const Navbar = () => {
     nav.innerHTML=`
     <div class="navdiv">
             <a href="/index.html"><img src="/fulllogo.png" class="logo" alt="Navbar Logo"></a>
-            <div class="navthings">
-                <div class="search">
-                    <input type="text" class="searchbar" placeholder="Search items">
-                    <button class="searchbutton"><b>Search</b></button>
-                </div>
-            </div>
     </div>
     <ul class="linklist">
         <li class="link"><a href="/index.html">Home</a></li>

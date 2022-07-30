e39e81baccc589b8cd3d9f75826683a863a79834
Arowadas <arowadas@gmail.com>
2022-07-29 20:08:34 -0400

    SEARCHBAR CODE

--- navbar/script.js
+++ navbar/script.js
@@ -4,6 +4,12 @@ const Navbar = () => {
     <div class="navdiv">
             <a href="/index.html"><img src="/fulllogo.png" class="logo" alt="Navbar Logo"></a>
     </div>
+    <div class="navthings">
+        <div class="search">
+            <input type="text" class="searchbar" placeholder="Search items">
+            <button class="searchbutton"><b>Search</b></button>
+        </div>
+    </div>
     <ul class="linklist">
         <li class="link"><a href="/index.html">Home</a></li>
         <li class="link"><a href="/membercard">Member Card</a></li>

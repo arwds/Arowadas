da9c9155e7196f8379a69de41125bfd29f71c9ca
Arowadas <arowadas@gmail.com>
2022-12-10 16:29:53 -0500

    Part2SearchBar

--- Searchbar/script.js
+++ Searchbar/script.js
@@ -1,10 +1,22 @@
 const productTemplate = document.querySelector("[data-product-template]")
+const productContainer = document.querySelector("[data-product-container]")
 
 fetch("/productpages/products.json")
     .then(res => res.json())
     .then(data => {
-        data.forEach(user=>{
+        data.forEach(product=>{
             const card = productTemplate.content.cloneNode(true).children[0]
-            console.log(user)
+            
+            const name=card.querySelector("[data-name]")
+            const price=card.querySelector("[data-price]")
+            const link=card.querySelector("[data-link]")
+            const image=card.querySelector("[data-image]")
+
+            name.textContent=product.name
+            price.textContent=product.price
+            link=product.link
+            image=product.image
+
+            productContainer.append(card)
         })
     })

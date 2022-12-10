4749ac634041760fd8a43ba1627020bd17e6ab38
Arowadas <arowadas@gmail.com>
2022-12-10 11:32:08 -0500

    Update script.js

--- Searchbar/script.js
+++ Searchbar/script.js
@@ -2,9 +2,9 @@ const productTemplate = document.querySelector("[data-product-template]")
 
 fetch("/productpages/products.json")
     .then(res => res.json())
-    .then(productdata => {
-        productdata.forEach(user=>{
-            const card = productTemplate.content.cloneNode(true).children(0)
-            console.log(user)
+    .then(data => {
+        data.forEach(user=>{
+            const card = productTemplate.content.cloneNode(true).children[0]
+            console.log(card)
         })
     })

9996fda886ed1e6dd473a87c10f466431a2b09fb
Arowadas <arowadas@gmail.com>
2022-12-10 18:46:02 -0500

    Update script.js

--- Searchbar/script.js
+++ Searchbar/script.js
@@ -1,26 +1,26 @@
-const productTemplate = document.querySelector("[data-product-template]")
-const productContainer = document.querySelector("[data-product-container]")
+const productTemplate = document.querySelector("[data-product-template]");
+const productContainer = document.querySelector("[data-product-container]");
 
 fetch("/productpages/products.json")
     .then(res => res.json())
     .then(data => {
         data.forEach(product=>{
-            const card = productTemplate.content.cloneNode(true).children[0]
+            const card = productTemplate.content.cloneNode(true).children[0];
             
-            const name=card.querySelector("[data-name]")
-            const price=card.querySelector("[data-price]")
-            const link=card.querySelector("[data-link]")
-            const image=card.querySelector("[data-image]")
+            const name=card.querySelector("[data-name]");
+            const price=card.querySelector("[data-price]");
+            const link=card.querySelector("[data-link]");
+            const image=card.querySelector("[data-image]");
 
-            name.textContent=product.name
-            price.textContent=product.price
-            image.src=product.image
+            name.textContent=product.name;
+            price.textContent=product.price;
+            image.src=product.image;
 
-            link.onclick=function(){
+            link.addEventListener("click", () => {
                 window.open(product.link, "_top")
-            }
+            })
 
-            console.log(card)
-            productContainer.append(card)
-        })
-    })
+            console.log(card);
+            productContainer.append(card);
+        });
+    });

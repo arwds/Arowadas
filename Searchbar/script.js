17bc8f301c1a6c143c2e756e0cc5126db86c9859
rextv <arowadas@gmail.com>
2023-03-18 09:29:28 -0400

    fixid products

--- Searchbar/script.js
+++ Searchbar/script.js
@@ -1,17 +1,34 @@
 import productinfo from "/productpages/products.json" assert {type: "json"};
+const SearchBar = document.querySelector("[data-search]")
+
+let prodcutnames = []
+
+
+//fetch("/productpages/products.json")
+//.then(resp => resp.json())
+//.then(data => console.log(data))
+
+for (var i in productinfo.jsonData){
+    console.log(Object.values(i));
+}
+
+SearchBar.addEventListener("input", inp =>{
+    const value = inp.target.value;
+
+});
 
 document.getElementById("container").innerHTML = `
     ${productinfo.map(function(item){
         return`
-        <a href="${item.link}" class="productlink" data-link>
             <div class="itemgrid">
-                <div class="product" style="cursor: pointer;">
-                    <center><img src = "${item.image}" class="productimg" /></center>
-                    <center><p class="productname">${item.name}</p></center>
-                    <center><p class="pricetag">${item.price}</p></center>
-                </div>
+                <a href="${item.link}" class="productlink" data-link>
+                    <div class="product" style="cursor: pointer;">
+                        <center><img src = "${item.image}" class="productimg" /></center>
+                        <center><p class="productname">${item.name}</p></center>
+                        <center><p class="pricetag">${item.price}</p></center>
+                    </div>
+                </a>
             </div>
-        </a>
         `
     }).join("")}
 `;
--- navbar/script.js
+++ navbar/script.js
@@ -5,7 +5,7 @@ const Navbar = () => {
             <a href="/index.html"><img src="/fulllogo.png" class="logo" alt="Navbar Logo"></a>
             <div class="navthings">
                 <div class="search">
-                    <input type="text" class="searchbar" placeholder="Search items">
+                    <input type="text" class="searchbar" placeholder="Search items" data-search>
                     <button class="searchbutton"><b>Search</b></button>
                 </div>
             </div>
